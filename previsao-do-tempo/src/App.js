f76e609fc707a2b4d66fe270fee4207a48db4147
edpadua <ed.padua@gmail.com>
2023-03-02 16:23:08 -0300

    Leitura e exibição dos dados da API openweathermap,
    Leitura exibição dos dados da API openweathermap utilizando a biblioteca Axios

--- previsao-do-tempo/src/App.js
+++ previsao-do-tempo/src/App.js
@@ -9,8 +9,8 @@ import PrevisaoTempo from './Componentes/PrevisaoTempo';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header className="d-flex justify-content-center align-items-center">
+                <h2>Previsao do Tempo e Clima</h2>
       </header>
       <PrevisaoTempo/>
     </div>
--- previsao-do-tempo/src/Componentes/PrevisaoTempo/index.js
+++ previsao-do-tempo/src/Componentes/PrevisaoTempo/index.js
@@ -1,26 +1,44 @@
 import "./PrevisaoTempo.css";
 import { useState, useEffect } from 'react';
+import axios from "axios";
 
-
+import countries from 'i18n-iso-countries';
 
 
 function PrevisaoTempo() {
 
 
-    const [apiData, setApiData] = useState({});
+
+    const [apiDataClima, setApiDataClima] = useState({});
+    const [apiDataPrevisao, setApiDataPrevisao] = useState({});
     const [getState, setGetState] = useState('');
     const [state, setState] = useState('');
 
 
-
     const apiKey = process.env.REACT_APP_API_KEY;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
+    const apiClimaUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}&lang=pt_br`;
+    const apiPrevisaoUrl = `https://api.openweathermap.org/data/2.5/forecast?&q=${state}&APPID=${apiKey}`
+
+
+    const getWeatherInfo = async () => {
+        const { data } = await axios.get(apiClimaUrl);
+        setApiDataClima(data);
+    };
+
+    const getForecastInfo = async () => {
+        const { data } = await axios.get(apiPrevisaoUrl);
+        setApiDataPrevisao(data);
+    };
+
 
     useEffect(() => {
-        fetch(apiUrl)
-            .then((res) => res.json())
-            .then((data) => setApiData(data));
-    }, [apiUrl]);
+        getForecastInfo();
+    }, [apiPrevisaoUrl]);
+
+
+    useEffect(() => {
+        getWeatherInfo();
+    }, [apiClimaUrl]);
 
 
     const inputHandler = (event) => {
@@ -31,32 +49,103 @@ function PrevisaoTempo() {
         setState(getState);
     };
 
+    const kelvinToFarenheit = (k) => {
+        return (k - 273.15).toFixed(2);
+    };
+
     return (
-        <div>
-            <form>
-                <div >
-                    <label for="nome-localizacao" >
-                        Digite o nome da cidade :
-                    </label>
-                </div>
-                <div>
+
+
+        <div >
+            <div >
+
+
+                <div class="col-auto">
                     <input
                         type="text"
-                        id="nome-localizacao"
+                        id="location-name"
+                        class="form-control"
                         onChange={inputHandler}
                         value={getState}
+                        placeholder="Digite a localização"
                     />
                 </div>
-                &nbsp; &nbsp; &nbsp;&nbsp;
-
                 <button className="btn btn-primary mt-2" onClick={submitHandler}>
                     Procurar
                 </button>
-            </form>
+            </div>
+
+            <div  >
+                {apiDataClima.main ? (
+                    <div >
+                        <img
+                            src={`http://openweathermap.org/img/w/${apiDataClima.weather[0].icon}.png`}
+                            
+                        />
+
+                        <p >
+                            {kelvinToFarenheit(apiDataClima.main.temp)}&deg; C
+                        </p>
+
+                        <p >
+                            <i className="fas fa-map-marker-alt"></i>{' '}
+                            <strong>{apiDataClima.name}</strong>
+                        </p>
+
+                        <div >
+                            <div >
+                                <p>
+                                    <i class="fas fa-temperature-low "></i>{' '}
+                                    <strong>
+                                        {kelvinToFarenheit(apiDataClima.main.temp_min)}&deg; C
+                                    </strong>
+                                </p>
+                                <p>
+                                    <i className="fas fa-temperature-high"></i>{' '}
+                                    <strong>
+                                        {kelvinToFarenheit(apiDataClima.main.temp_max)}&deg; C
+                                    </strong>
+                                </p>
+                            </div>
+                            <div >
+                                <p>
+                                    {' '}
+                                    <strong>{apiDataClima.weather[0].main}</strong>
+                                </p>
+                                <p>
+                                    <strong>
+                                        {' '}
+                                        {countries.getName(apiDataClima.sys.country, 'pt', {
+                                            select: 'official',
+                                        })}
+                                    </strong>
+                                </p>
+                            </div>
+                        </div>
+                        {
+                            apiDataPrevisao.list.map((forecast, i) => (
+                                <div key={i}>
+                                    
+                                    <h4>{forecast.dt_txt}</h4>
+                                    <ul>
+                                        <li>Temperatura: {forecast.main.temp}</li>
+                                        <li>Umidade: {forecast.main.humidity}</li>
+                                    </ul>
+                                </div>
+                            ))}
+
+                    </div>
+
+
+
+                ) : (
+                    <h1></h1>
+                )}
+            </div>
         </div>
 
-    )
 
+    )
 }
 
 
